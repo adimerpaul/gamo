0ce92d8f49cc988efdd8873d56793ff00c8703ce
AFLopezG <lopez.gutierrez.a07@gmail.com>
2021-10-06 12:23:11 -0400

    rutas

--- gamofront/src/pages/Index.vue
+++ gamofront/src/pages/Index.vue
@@ -11,6 +11,12 @@
       <div class="col-12">
         <q-btn color="red-10" to="/tasas" icon="code" icon-right="send" label="Deudas tasas por servicio" />
       </div>
+      <div class="col-12">
+        <q-btn color="green-10" @click="vehiculo"  icon-right="send" label="Ir a consultar Deudas Ruat Vehiculos" />
+      </div>
+      <div class="col-12">
+        <q-btn color="green-10" @click="inmueble"  icon-right="send" label=" Ir a consultar Deudas Ruat Inmuebles" />
+      </div>
     </div>
 
 
@@ -19,7 +25,24 @@
 </template>
 
 <script>
+import { openURL } from 'quasar'
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+  created() {
+    // console.log(process.env.URL)
+  },
+  data () {
+    return{}
+  },
+   methods: {
+     vehiculo(){
+       openURL('https://www.ruat.gob.bo/vehiculos/consultageneral/InicioBusquedaVehiculo.jsf ');
+     },
+     inmueble(){
+       openURL('https://www.ruat.gob.bo/inmuebles/consultageneral/InicioBusquedaInmueble.jsf');
+     }
+   }
+
+ 
 }
 </script>
